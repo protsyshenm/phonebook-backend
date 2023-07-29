c34e3cb9e8e80fc370bceed9450529603e18c54e
Mykhailo Protsyshen <protsyshenm@gmail.com>
2023-07-29 21:03:11 +0300

    Solution for exercise 3.4

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const express = require('express')
 const app = express()
 
-const persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  },
+  {
+    "name": "Linus Torvalds",
+    "number": "12345678",
+    "id": 5
+  },
+  {
+    "name": "Robert Martin",
+    "number": "45654325",
+    "id": 6
+  },
+  {
+    "name": "Nancy Sinatra",
+    "number": "32131321",
+    "id": 7
   }
 ]
 
@@ -44,6 +59,13 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  
+  response.status(204).end()
 })
 
 const PORT = 3001
